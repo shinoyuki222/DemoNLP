41b2d0d8b2a04d65491d98e5e25c7c1d8143b0a2
shinoyuki222 <s1718004@sms.ed.ac.uk>
2020-01-26 12:52:22 +0800

    Update train.py

--- FreeChat/main_BERT_ChatBot/train.py
+++ FreeChat/main_BERT_ChatBot/train.py
@@ -35,11 +35,11 @@ def train(encoder, decoder, data_iterator, encoder_optimizer, decoder_optimizer,
     tqdm_t = trange(params.train_steps)
     for i in tqdm_t:
         # fetch the next training batch
-        batch_data, batch_tags = next(data_iterator)
+        batch_data, batch_answers, batch_max_len_target = next(data_iterator)
         batch_masks = batch_data.gt(0)
 
         # compute model output and loss
-        encoder_outputs, encoder_hidden = encoder(batch_data, token_type_ids=None, attention_mask=batch_masks, labels=batch_tags)
+        encoder_outputs, encoder_hidden = encoder(batch_data, token_type_ids=None, attention_mask=batch_masks)
         # Set initial decoder hidden state to the encoder's final hidden state
         decoder_hidden = encoder_hidden[:decoder.n_layers]
 
@@ -48,7 +48,7 @@ def train(encoder, decoder, data_iterator, encoder_optimizer, decoder_optimizer,
 
         # Forward batch of sequences through decoder one time step at a time
         if use_teacher_forcing:
-            for t in range(max_target_len):
+            for t in range(batch_max_len_target):
                 decoder_output, decoder_hidden = decoder(
                     decoder_input, decoder_hidden, encoder_outputs
                 )
@@ -60,14 +60,14 @@ def train(encoder, decoder, data_iterator, encoder_optimizer, decoder_optimizer,
                 print_losses.append(mask_loss.item() * nTotal)
                 n_totals += nTotal
         else:
-            for t in range(max_target_len):
+            for t in range(batch_max_len_target):
                 decoder_output, decoder_hidden = decoder(
                     decoder_input, decoder_hidden, encoder_outputs
                 )
                 # No teacher forcing: next input is decoder's own current output
                 _, topi = decoder_output.topk(1)
                 decoder_input = torch.LongTensor([[topi[i][0] for i in range(batch_size)]])
-                decoder_input = decoder_input.to(device)
+                decoder_input = decoder_input.to(params.device)
                 # Calculate and accumulate loss
                 mask_loss, nTotal = maskNLLLoss(decoder_output, target_variable[t], mask[t])
                 loss += mask_loss
@@ -82,25 +82,33 @@ def train(encoder, decoder, data_iterator, encoder_optimizer, decoder_optimizer,
             loss = loss.mean()  # mean() to average on multi-gpu
 
         # clear previous gradients, compute gradients of all variables wrt loss
-        model.zero_grad()
+        # Zero gradients
+        encoder_optimizer.zero_grad()
+        decoder_optimizer.zero_grad()
 
         if args.fp16:
-            optimizer.backward(loss)
+            encoder_optimizer.backward(loss)
+            decoder_optimizer.backward(loss)
         else:
             loss.backward()
 
-        # gradient clipping
-        nn.utils.clip_grad_norm_(parameters=model.parameters(), max_norm=params.clip_grad)
+
+        # Clip gradients: gradients are modified in place
+        _ = nn.utils.clip_grad_norm_(encoder.parameters(), max_norm=params.clip_grad)
+        _ = nn.utils.clip_grad_norm_(decoder.parameters(), max_norm=params.clip_grad)
+
 
         # performs updates using calculated gradients
-        optimizer.step()
+        # Adjust model weights
+        encoder_optimizer.step()
+        decoder_optimizer.step()
 
         # update the average loss
         loss_avg.update(loss.item())
-        t.set_postfix(loss='{:05.3f}'.format(loss_avg()))
+        tqdm_t.set_postfix(loss='{:05.3f}'.format(loss_avg()))
     
 
-def train_and_evaluate(encoder, decoder,, train_data, val_data, encoder_optimizer, decoder_optimizer, scheduler, params, model_dir, restore_file=None):
+def train_and_evaluate(encoder, decoder, train_data, val_data, encoder_optimizer, decoder_optimizer, scheduler, params, model_dir, restore_file=None):
     """Train the model and evaluate every epoch."""
     # reload weights from restore_file if specified
     if restore_file is not None:
@@ -108,7 +116,8 @@ def train_and_evaluate(encoder, decoder,, train_data, val_data, encoder_optimize
         logging.info("Restoring parameters from {}".format(restore_path))
         utils.load_checkpoint(restore_path, encoder, encoder_optimizer)
         
-    best_val_f1 = 0.0
+    # best_val_f1 = 0.0
+    best_val = 0.0
     patience_counter = 0
 
     for epoch in range(1, params.epoch_num + 1):
@@ -134,8 +143,10 @@ def train_and_evaluate(encoder, decoder,, train_data, val_data, encoder_optimize
         params.eval_steps = params.val_steps
         val_metrics = evaluate(encoder, decoder, val_data_iterator, params, mark='Dev')
         
-        val_f1 = val_metrics['f1']
-        improve_f1 = val_f1 - best_val_f1
+        val_loss = val_metrics
+        improve_loss = val_loss-best_val
+        # val_f1 = val_metrics['f1']
+        # improve_f1 = val_f1 - best_val_f1
 
         # Save weights of the network
         model_to_save = model.module if hasattr(model, 'module') else model  # Only save the model it-self
@@ -143,12 +154,12 @@ def train_and_evaluate(encoder, decoder,, train_data, val_data, encoder_optimize
         utils.save_checkpoint({'epoch': epoch + 1,
                                'state_dict': model_to_save.state_dict(),
                                'optim_dict': optimizer_to_save.state_dict()},
-                               is_best=improve_f1>0,
+                               is_best=improve_loss>0,
                                checkpoint=model_dir)
-        if improve_f1 > 0:
+        if improve_loss > 0:
             logging.info("- Found new best F1")
-            best_val_f1 = val_f1
-            if improve_f1 < params.patience:
+            best_val = val_loss
+            if  improve_loss < params.patience:
                 patience_counter += 1
             else:
                 patience_counter = 0
@@ -157,17 +168,17 @@ def train_and_evaluate(encoder, decoder,, train_data, val_data, encoder_optimize
 
         # Early stopping and logging best f1
         if (patience_counter >= params.patience_num and epoch > params.min_epoch_num) or epoch == params.epoch_num:
-            logging.info("Best val f1: {:05.2f}".format(best_val_f1))
+            logging.info("Best val loss: {:05.2f}".format(best_val))
             break
         
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', default='..\\chatbot_data\\QA', help="Directory containing the dataset")
+    parser.add_argument('--data_dir', default='QA', help="Directory containing the dataset")
     parser.add_argument('--bert_model_dir', default='bert-base-chinese-pytorch',
                         help="Directory containing the BERT model in PyTorch")
-    parser.add_argument('--model_dir', default='experiments\\base_model', help="Directory containing params.json")
+    parser.add_argument('--model_dir', default='base_model', help="Directory containing params.json")
     parser.add_argument('--seed', type=int, default=2019, help="random seed for initialization")
     parser.add_argument('--restore_file', default=None,
                         help="Optional, name of the file in --model_dir containing weights to reload before training")
@@ -185,7 +196,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Load the parameters from json file
-    json_path = os.path.join(args.model_dir, 'params.json')
+    json_path = os.path.join('experiments', args.model_dir, 'params.json')
     bert_config_path = os.path.join(args.bert_model_dir, 'bert_config.json')
     assert os.path.isfile(json_path), "No json configuration file found at {}".format(json_path)
     params = utils.Params(json_path)
@@ -203,14 +214,15 @@ if __name__ == '__main__':
     params.seed = args.seed
     
     # Set the logger
-    utils.set_logger(os.path.join(args.model_dir, 'train.log'))
+    utils.set_logger(os.path.join('experiments', args.model_dir, 'train.log'))
     logging.info("device: {}, n_gpu: {}, 16-bits training: {}".format(params.device, params.n_gpu, args.fp16))
 
     # Create the input data pipeline
     logging.info("Loading the datasets...")
     
     # Initialize the DataLoader
-    data_loader = DataLoader(args.data_dir, args.bert_model_dir, params, token_pad_idx=0)
+    data_dir = os.path.join('..','chatbot_data',args.data_dir)
+    data_loader = DataLoader(data_dir, args.bert_model_dir, params, token_pad_idx=0)
     
     # Load training data and test data
     train_data = data_loader.load_data('train')
@@ -272,11 +284,11 @@ if __name__ == '__main__':
     attn_model = 'dot'
     decoder_n_layers = 2
     decoder = LuongAttnDecoderRNN(attn_model, encoder.embeddings, bert_config.hidden_size, bert_config.vocab_size, decoder_n_layers, bert_config.hidden_dropout_prob)
-    decoder.to(device)
-    decoder_optimizer = optim.Adam(decoder.parameters(), lr=params.learning_rate * params.decoder_learning_ratio)
+    decoder.to(params.device)
+    decoder_optimizer = Adam(decoder.parameters(), lr=params.learning_rate * params.decoder_learning_ratio)
 
     # If you have cuda, configure cuda to call
-    if device == 'cuda':
+    if params.device == 'cuda':
         for state in decoder_optimizer.state.values():
             for k, v in state.items():
                 if isinstance(v, torch.Tensor):
